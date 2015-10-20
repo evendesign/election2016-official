f0be0a30b1ab05c93b14bb6fdcf3c921fb0b4e95
shengli1989 <shengli@evendesign.tw>
2015-10-20 16:40:04 +0800

    解決在手機播放影片時，回到桌面版 cover 還在的問題

--- javascripts/all.js
+++ javascripts/all.js
@@ -193,38 +193,31 @@ $( document ).ready(function() {
   });
 });
 
-// https://developers.google.com/youtube/iframe_api_reference
 
-// Inject YouTube API script
+
+// youtube custom cover
 var tag = document.createElement('script');
 tag.src = "//www.youtube.com/player_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// global variable for the player
 var player;
-
-// this function gets called when API is ready to use
 function onYouTubePlayerAPIReady() {
-  // create the global player from the specific iframe (#video)
   player = new YT.Player('video', {
     events: {
-      // call this function when player is ready to use
-      'onReady': onPlayerReady
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
     }
   });
 }
-
 function onPlayerReady(event) {
-  // bind events
   var playButton = document.getElementById("play-btn");
   playButton.addEventListener("click", function() {
     player.playVideo();
-    $('.post-cover').toggleClass('is-playing');
+    $('.post-cover').addClass('is-playing');
   });
-
-  var pauseButton = document.getElementById("pause-button");
-  pauseButton.addEventListener("click", function() {
-    player.pauseVideo();
-  });
+}
+function onPlayerStateChange(event) {
+  if (event.data == 1) {
+    $('.post-cover').addClass('is-playing');
+  }
 }

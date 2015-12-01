dce4625bd988af5f501ca12e2e5e950831501097
shengli1989 <shengli@evendesign.tw>
2015-12-01 16:25:22 +0800

    更新 modernize

--- javascripts/all.js
+++ javascripts/all.js
@@ -186,28 +186,30 @@ $( document ).ready(function() {
 });
 
 // youtube custom cover
-var tag = document.createElement('script');
-tag.src = "//www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-var player;
-function onYouTubePlayerAPIReady() {
-  player = new YT.Player('video', {
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
+if (!Modernizr.touchevents) {
+  var tag = document.createElement('script');
+  tag.src = "//www.youtube.com/player_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  var player;
+  function onYouTubePlayerAPIReady() {
+    player = new YT.Player('video', {
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
+    });
+  }
+  function onPlayerReady(event) {
+    var playButton = document.getElementById("play-btn");
+    playButton.addEventListener("click", function() {
+      player.playVideo();
+      $('.post-cover').addClass('is-playing');
+    });
+  }
+  function onPlayerStateChange(event) {
+    if (event.data == 1) {
+      $('.post-cover').addClass('is-playing');
     }
-  });
-}
-function onPlayerReady(event) {
-  var playButton = document.getElementById("play-btn");
-  playButton.addEventListener("click", function() {
-    player.playVideo();
-    $('.post-cover').addClass('is-playing');
-  });
-}
-function onPlayerStateChange(event) {
-  if (event.data == 1) {
-    $('.post-cover').addClass('is-playing');
   }
 }

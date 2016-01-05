c6b30c2397678ac54b19c8f90eaa13c5526dc925
TouNeko <touneko@gmail.com>
2016-01-05 18:40:30 +0800

    調整倒數內容

--- javascripts/countdown.js
+++ javascripts/countdown.js
@@ -2,13 +2,13 @@ var electionTime = moment.tz("2016-01-16 08:00", "Asia/Taipei");
 
 $('.countdown').countdown(electionTime.toDate()).on('update.countdown', function(event) {
   $(this).html(event.strftime(''
-    + '<div class="item"><div class="unit">DAYS</div><div class="number">%D</div></div>'
+    + '<div class="item"><div class="number">%D</div><div class="unit">DAYS</div></div>'
     + '<div class="colon">：</div>'
-    + '<div class="item"><div class="unit">HOURS</div><div class="number">%H</div></div>'
+    + '<div class="item"><div class="number">%H</div><div class="unit">HOURS</div></div>'
     + '<div class="colon">：</div>'
-    + '<div class="item"><div class="unit">MINS</div><div class="number">%M</div></div>'
+    + '<div class="item"><div class="number">%M</div><div class="unit">MINS</div></div>'
     + '<div class="colon">：</div>'
-    + '<div class="item"><div class="unit">SECS</div><div class="number">%S</div></div>'
+    + '<div class="item"><div class="number">%S</div><div class="unit">SECS</div></div>'
   ));
 }).on('finish.countdown', function(event) {
     console.log('finfsh');
